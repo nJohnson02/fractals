1a5478360a928c268e8f3b2c8f74ac433522dccc
Dapp3rDuck <mc.choof15@gmail.com>
2020-01-03 22:37:44 -0700

    More Grid

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ print('Input Fractal in terms of z, reals, and c, complex:')
 FRACTAL = str(input('z = '))
 ITERATIONS = int(input('Iterations: '))
 WIDTH, HEIGHT = int(input('Width: ')), int(input('Height: '))
+GRID = str(input('Display Grid (y/n): '))
+if GRID == 'y':
+    NUM = int(input('Grid Size: '))
+else:
+    NUM = 1
 
 x1, x2 = float(input('Domain Start: ')), float(input('Domain End: '))
 y1, y2 = -1 * float(input('Range Start: ')), -1 * float(input('Range End: '))
@@ -22,10 +27,10 @@ def fractal(x, y):
     c = complex(x1+x*A, y1+y*B)
     c = c
     z = 0
-    if ((x%(img.size[0]/5)) != 0) and ((y%(img.size[1]/5)) != 0) and y != -1*(y2)-50:
+    if ((x%(img.size[0]/NUM)) != 0) and (((y%(img.size[1]/NUM)) != 0)) and (y != (y2/B)-1)  and (x != (x2/A)-1) or (GRID == 'n'):
         for i in range(1, ITERATIONS):
             if abs(z) > 2: return rgb_conv(i)
-            z = eval(FRACTAL) 
+            z = eval(FRACTAL)
         return (0, 0, 0)
     else:
         return (200, 200, 200)
