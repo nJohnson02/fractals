61f042ebebce96c78071c48ccb45b694b81dbcc4
Ryan Harrington <rharrington.web@gmail.com>
2020-01-03 21:24:49 -0700

    added .gitignore + minor cleanup

--- main.py
+++ main.py
@@ -4,25 +4,28 @@ import colorsys
 import math
 
 print('Input Fractal in terms of z, reals, and c, complex:')
+
 FRACTAL = str(input('z = '))
 ITERATIONS = int(input('Iterations: '))
 WIDTH, HEIGHT = int(input('Width: ')), int(input('Height: '))
-x1, x2 = float(input('Domain Start: ')), float(input('Domain End: '))
-y1, y2 = -1*float(input('Range Start: ')), -1*float(input('Range End: '))
 
-A, B = (x2 - x1)/WIDTH, (y2 - y1)/HEIGHT
+x1, x2 = float(input('Domain Start: ')), float(input('Domain End: '))
+y1, y2 = -1 * float(input('Range Start: ')), -1 * float(input('Range End: '))
+
+A, B = (x2 - x1) / WIDTH, (y2 - y1) / HEIGHT
 
 def rgb_conv(i):
     color = 255 * array(colorsys.hsv_to_rgb(i / 255.0, 1.0, 0.5))
     return tuple(color.astype(int))
 
 def fractal(x, y):
-    c = complex(x1+x*A, y1+y*B)
-    c = c
-    z = 0
+
+    z, c = 0, complex(x1 + x * A, y1 + y * B)
+    
     for i in range(1, ITERATIONS):
         if abs(z) > 2: return rgb_conv(i)
         z = eval(FRACTAL) 
+
     return (0, 0, 0)
 
 img = Image.new('RGB', (WIDTH, HEIGHT))
