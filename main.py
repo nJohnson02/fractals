f1b482c6a5fc68af5785202dbcccc5f567f3d53e
Dapp3rDuck <mc.choof15@gmail.com>
2020-01-09 09:14:37 -0700

    Bar

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def progress(count, total, status=''):
     filled_len = int(round(bar_len * count / float(total)))
 
     percents = round(100.0 * count / float(total), 1)
-    bar = '▒' * filled_len + '-' * (bar_len - filled_len)
+    bar = '▧' * filled_len + '□' * (bar_len - filled_len)
 
     sys.stdout.write('%s [%s] %s%s \r' % (status, bar, percents, '%'))
     sys.stdout.flush()
